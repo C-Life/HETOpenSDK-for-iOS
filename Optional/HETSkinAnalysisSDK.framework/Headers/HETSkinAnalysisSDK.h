86e491ca7acf99cddde79d695b60afddd4e17c7a
袁云龙 <yuanyunlong@yuanyunlongdeMacBook-Pro.local>
2019-02-15 11:12:58 +0800

    add HETSkinAnalysis

---
+++ Optional/HETSkinAnalysisSDK.framework/Headers/HETSkinAnalysisSDK.h
@@ -0,0 +1,20 @@
+//
+//  HETSkinAnalysisSDK.h
+//  HETSkinAnalysisSDK
+//
+//  Created by 袁云龙 on 2019/2/13.
+//  Copyright © 2019年 袁云龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HETSkinAnalysisSDK.
+FOUNDATION_EXPORT double HETSkinAnalysisSDKVersionNumber;
+
+//! Project version string for HETSkinAnalysisSDK.
+FOUNDATION_EXPORT const unsigned char HETSkinAnalysisSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HETSkinAnalysisSDK/PublicHeader.h>
+
+#import "HETCBSkinAnalysis.h"
+#import "HETCBSkinCameraTool.h"
